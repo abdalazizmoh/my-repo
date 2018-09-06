cef43235ccd52f72abcadf95727ba9d1ddc484f3
Abdalaziz Mohamed <abdalaziz.ali87@gmail.com>
2018-09-06 19:13:04 +0300

    Add existing file

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php	
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
+?>
+<H1>Hello World!</H1>
+<div>
+<?php 
+ echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;
+ ?>
+</div>
+<div>
+<?php 
+ echo "Version 1";
+ ?>
+</div>
